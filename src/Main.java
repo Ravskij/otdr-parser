e7c765727019cdb64881ee40b5fa0b064036e1ee
Andrew <ravski99@gmail.com>
2025-01-04 11:18:54 +0300

    small fixes

--- src/Main.java
+++ src/Main.java
@@ -2,80 +2,84 @@ import java.io.IOException;
 import java.io.FileInputStream;
 
 public class Main {
-    private static final int KEY_EVENTS_OFFSET = 18; // Смещение по KeyEvents от K... до ...s
-    private static final int KEY_EVENTS_SKIP_BYTE = 2; // Пропуск 1 байта (2 символа)
-    private static final int KEY_EVENTS_MIN_OFFSET = 86; // Минимальное расстояние между событиями
+    private static final int KEY_EVENTS_START_BLOCK = 18; // Смещение до "s" в "KeyEvents"
+    private static final int KEY_EVENTS_OFFSET = 86; // Минимальное расстояние между событиями
+    private static final int SKIP_BYTE = 2; // Пропуск двух символов (1 байт)
 
     public static void main(String[] args) {
 
+        int cursor; // Курсор для перемещения по бинарному файлу
+        int countEvents; // Количество событий в блоке KeyEvents
+        String[] arrayEvent; // Массив событий KeyEvents
+        String hexNumber; // десятичное число, приведенное к шестнадцатеричному
+
         String filePath = "OTDRs\\Modified OTDR.sor";
+        String fileContent = fileReader(filePath);
 
-        // Получение данных с использованием FileInputStream
-        // Данные в файле записаны побайтово: 1 байт - 2 символа в 16-ричной системе.
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            String fileContent = "";
-            while (fileInputStream.available() != 0) {
-                fileContent = fileContent + String.format("%02X", fileInputStream.read());
-            }
+        if (fileContent != null) {
+            // Преобразование строки "KeyEvents" к определенному виду для дальнейшего поиска блока KeyEvents по файлу
+            cursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_START_BLOCK + SKIP_BYTE;
+            countEvents = compareToDec(hexScanner(fileContent, cursor, 1));
 
-            // Преобразование строки "KeyEvents" в необходимый вид, для дальнейшего поиска блока KeyEvents по файлу.
-            // Поиск второго(последнего) появление "KeyEvents". KEY_EVENTS_OFFSET(... + 18) - позиция последнего символа "...s"
-            int cursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_OFFSET + KEY_EVENTS_SKIP_BYTE;
-            // Приведение строки с 2 символами в 16-ричной системе к целому числу в 10-чной системе.
-            int countEvents = compareToDec(hexScanner(fileContent, cursor, 1));
-            System.out.println("Всего зафиксированных событий: " + countEvents);
-
-            // Структура (какие-то данные у которых есть номер события)
-            // Сделать через коллекцию. Потом...
-            String[] arrayEvent = new String[countEvents];
+            // Первое событие находится около курсора, перемещаем его на безопасное расстояние
+            cursor -= KEY_EVENTS_OFFSET;
+            arrayEvent = new String[countEvents];
             for (int i = 0; i < countEvents; i++) {
-                String currentEventNumberHex = "00" + compareToHex(i + 1) + "00";
-                if (i == 0) {
-                    cursor = fileContent.indexOf(currentEventNumberHex, cursor);
-                } else {
-                    cursor = fileContent.indexOf(currentEventNumberHex, cursor + KEY_EVENTS_MIN_OFFSET);
-                }
-
-                arrayEvent[i] = String.valueOf(hexScanner(fileContent, cursor + KEY_EVENTS_SKIP_BYTE, 1));
+                hexNumber = "00" + compareToHex(i + 1) + "00";
+                cursor = fileContent.indexOf(hexNumber, cursor + KEY_EVENTS_OFFSET);
+                arrayEvent[i] = hexScanner(fileContent, cursor + SKIP_BYTE, 1);
                 System.out.println("Событие №" + i + " = " + arrayEvent[i]);
             }
 
+            System.out.println("Всего зафиксированных событий: " + countEvents);
 
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        } else {
+            System.out.println("Файл не найден или не удалось прочитать файл");
         }
-
     }
 
     // Метод преобразования String в String вида HEX
     public static String compareToHex(String inputString) {
-        String hexString = "";
+        StringBuilder hexContent = new StringBuilder();
         for (int i = 0; i < inputString.length(); i++) {
-            hexString = hexString + String.format("%02X", (int)inputString.charAt(i));
+            hexContent.append(String.format("%02X", (int)inputString.charAt(i)));
         }
-        return hexString;
+        return hexContent.toString();
     }
 
     // Метод преобразования int в String вида HEX
     public static String compareToHex(int inputInt) {
-        String hexString = "";
-        hexString = hexString + String.format("%02X", inputInt);
-        return hexString;
+        return String.format("%02X", inputInt);
     }
 
-    // Метод преобразования HEX String в int
+    // Метод приведение строки с 2 символами в 16-ричной системе к целому числу в 10-чной системе.
     public static int compareToDec(String hexString) {
         return Integer.parseInt(hexString, 16);
     }
 
-    // Метод выводит N количество символов из основной строки начиная с определенного значения
+    // Метод выводит N-ое количество символов из основной строки начиная с определенного значения
     public static String hexScanner(String fileContent, int cursor, int readBytes) {
-        String hexString = "";
-        for (int i = 0; i < readBytes * 2; i += 2) {
-            hexString = hexString + fileContent.charAt(cursor + i) + fileContent.charAt(cursor + i + 1);
+        StringBuilder hexContent = new StringBuilder();
+        for (int i = 0; i < readBytes * SKIP_BYTE; i += SKIP_BYTE) {
+            hexContent.append(fileContent.charAt(cursor + i) + fileContent.charAt(cursor + i + 1));
         }
-        return hexString;
+        return hexContent.toString();
+    }
+
+    // Метод чтения бинарного файла и записи содержимого в строку
+    public static String fileReader(String path) {
+        // Получение данных с использованием FileInputStream
+        // Данные в файле записаны побайтово: 1 байт - 2 символа в 16-ричной системе.
+        try (FileInputStream fileInputStream = new FileInputStream(path)) {
+            String fileContent = "";
+            while (fileInputStream.available() != 0) {
+                fileContent = fileContent + String.format("%02X", fileInputStream.read());
+            }
+            return fileContent;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
 }
