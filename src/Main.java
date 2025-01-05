7cd06188bde843107a1f8882440f7603d135337d
Andrew <ravski99@gmail.com>
2025-01-05 14:23:26 +0300

    add methods inverting and fliping hex data

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,46 @@ public class Main {
     private static final int KEY_EVENTS_START_BLOCK = 18; // Смещение до "s" в "KeyEvents"
     private static final int KEY_EVENTS_OFFSET = 86; // Минимальное расстояние между событиями
     private static final int SKIP_BYTE = 2; // Пропуск двух символов (1 байт)
+    private static final int SHORT_INT_HEX = 4;
+    private static final int LONG_INT_HEX = 8;
 
     public static void main(String[] args) {
 
         int cursor; // Курсор для перемещения по бинарному файлу
+        int eventCursor; // Курсор начала события
         int countEvents; // Количество событий в блоке KeyEvents
+        String hexNumber; // Десятичное число, приведенное к шестнадцатеричному
         String[] arrayEvent; // Массив событий KeyEvents
-        String hexNumber; // десятичное число, приведенное к шестнадцатеричному
 
-        String filePath = "OTDRs\\Modified OTDR.sor";
+        String filePath = "C:\\Users\\Andrey\\Desktop\\st_B_20241 — копия.sor";
         String fileContent = fileReader(filePath);
 
         if (fileContent != null) {
             // Преобразование строки "KeyEvents" к определенному виду для дальнейшего поиска блока KeyEvents по файлу
-            cursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_START_BLOCK + SKIP_BYTE;
-            countEvents = compareToDec(hexScanner(fileContent, cursor, 1));
+            eventCursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_START_BLOCK + SKIP_BYTE;
+            countEvents = (int)compareToDec(hexScanner(fileContent, eventCursor, 1));
 
             // Первое событие находится около курсора, перемещаем его на безопасное расстояние
-            cursor -= KEY_EVENTS_OFFSET;
+            eventCursor -= KEY_EVENTS_OFFSET;;
             arrayEvent = new String[countEvents];
             for (int i = 0; i < countEvents; i++) {
-                hexNumber = "00" + compareToHex(i + 1) + "00";
-                cursor = fileContent.indexOf(hexNumber, cursor + KEY_EVENTS_OFFSET);
-                arrayEvent[i] = hexScanner(fileContent, cursor + SKIP_BYTE, 1);
-                System.out.println("Событие №" + i + " = " + arrayEvent[i]);
-            }
+                arrayEvent[i] = "";
+                hexNumber = compareToHex(i + 1, SHORT_INT_HEX);
+                eventCursor = cursor = fileContent.indexOf(hexNumber, eventCursor + KEY_EVENTS_OFFSET);
+                arrayEvent[i] += hexScanner(fileContent, cursor, SHORT_INT_HEX) + " ";
+                cursor += SHORT_INT_HEX;
+                arrayEvent[i] += hexScanner(fileContent, cursor, LONG_INT_HEX) + " ";
+                cursor += LONG_INT_HEX;
+                arrayEvent[i] += (double)compareToDec(hexScanner(fileContent, cursor, SHORT_INT_HEX)) / 1000 + " ";
+                cursor += SHORT_INT_HEX;
+                arrayEvent[i] += (double)compareToDec(hexScanner(fileContent, cursor, SHORT_INT_HEX)) / 1000 + " ";
+                cursor += SHORT_INT_HEX + SKIP_BYTE;
+                arrayEvent[i] += -(double)compareToDec(invertHex(flipHex(hexScanner(fileContent, cursor, LONG_INT_HEX)))) / 1000 + " ";
+                cursor += LONG_INT_HEX;
+                System.out.println("Событие №" + arrayEvent[i]);
 
+
+            }
             System.out.println("Всего зафиксированных событий: " + countEvents);
 
         } else {
@@ -47,39 +61,55 @@ public class Main {
         return hexContent.toString();
     }
 
-    // Метод преобразования int в String вида HEX
-    public static String compareToHex(int inputInt) {
-        return String.format("%02X", inputInt);
+    // Преобразование int в String вида HEX
+    public static String compareToHex(int inputInt, int countSymbols) {
+        return String.format("%0" + countSymbols + "X", inputInt);
     }
 
-    // Метод приведение строки с 2 символами в 16-ричной системе к целому числу в 10-чной системе.
-    public static int compareToDec(String hexString) {
-        return Integer.parseInt(hexString, 16);
+    // Приведение строки с 2 символами в 16-ричной системе к целому числу в 10-чной системе.
+    public static long compareToDec(String hexString) {
+        return Long.parseLong(hexString, 16);
     }
 
-    // Метод выводит N-ое количество символов из основной строки начиная с определенного значения
+    // Вывод N-ого количества символов из основной строки начиная с определенного значения
     public static String hexScanner(String fileContent, int cursor, int readBytes) {
         StringBuilder hexContent = new StringBuilder();
-        for (int i = 0; i < readBytes * SKIP_BYTE; i += SKIP_BYTE) {
-            hexContent.append(fileContent.charAt(cursor + i) + fileContent.charAt(cursor + i + 1));
+        for (int i = 0; i < readBytes; i += SKIP_BYTE) {
+            hexContent.append(fileContent.charAt(cursor + i));
+            hexContent.append(fileContent.charAt(cursor + i + 1));
         }
         return hexContent.toString();
     }
 
-    // Метод чтения бинарного файла и записи содержимого в строку
+    // Чтение данных из бинарного файла и запись содержимого в строку
     public static String fileReader(String path) {
-        // Получение данных с использованием FileInputStream
         // Данные в файле записаны побайтово: 1 байт - 2 символа в 16-ричной системе.
         try (FileInputStream fileInputStream = new FileInputStream(path)) {
-            String fileContent = "";
+            StringBuilder fileContent = new StringBuilder();
             while (fileInputStream.available() != 0) {
-                fileContent = fileContent + String.format("%02X", fileInputStream.read());
+                fileContent.append(String.format("%02X", fileInputStream.read()));
             }
-            return fileContent;
+            return fileContent.toString();
         } catch (IOException e) {
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    // Побайтовый переворот 16-ричного числа
+    public static String flipHex(String hexNumber) {
+        StringBuilder negativeHexNumber = new StringBuilder();
+        for (int i = hexNumber.length() - 1; i > 0; i-= SKIP_BYTE) {
+            negativeHexNumber.append(hexNumber.charAt(i - 1));
+            negativeHexNumber.append(hexNumber.charAt(i));
+        }
+        return negativeHexNumber.toString();
+    }
+
+    // Побитовое инвертирование 16-ричного числа
+    public static String invertHex(String hexNumber) {
+        long value = ~compareToDec(hexNumber) + 1 & 0xFFFFFFFFL;
+        return compareToHex((int)value, LONG_INT_HEX);
     }
 
 }
