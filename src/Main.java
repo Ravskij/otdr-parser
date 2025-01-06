3cd462b4c0d4c7b14250d77a21c8618dabf813f2
Andrew <ravski99@gmail.com>
2025-01-06 13:52:04 +0300

    added distance to event and loss on event

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,28 @@ import java.io.FileInputStream;
 public class Main {
     private static final int KEY_EVENTS_START_BLOCK = 18; // Смещение до "s" в "KeyEvents"
     private static final int KEY_EVENTS_OFFSET = 86; // Минимальное расстояние между событиями
-    private static final int SKIP_BYTE = 2; // Пропуск двух символов (1 байт)
+    private static final int ONE_BYTE = 2; // Пропуск или считывание двух символов (1 байт)
     private static final int SHORT_INT_HEX = 4;
     private static final int LONG_INT_HEX = 8;
+    private static final double SPEED_OF_LIGHT = 299_792_458; // в м/с
 
     public static void main(String[] args) {
 
+        int countEvents; // Общее количество событий в блоке KeyEvents
         int cursor; // Курсор для перемещения по бинарному файлу
-        int eventCursor; // Курсор начала события
-        int countEvents; // Количество событий в блоке KeyEvents
+        int eventCursor; // Курсор начала каждого события
+        double transitTime; // Время распространения импульса в с
+        double refractionIndex = 1.4682; // Коэффициент преломления
+
         String hexNumber; // Десятичное число, приведенное к шестнадцатеричному
         String[] arrayEvent; // Массив событий KeyEvents
 
-        String filePath = "C:\\Users\\Andrey\\Desktop\\st_B_20241 — копия.sor";
+        String filePath = "OTDRs/Modified OTDR.sor";
         String fileContent = fileReader(filePath);
 
         if (fileContent != null) {
             // Преобразование строки "KeyEvents" к определенному виду для дальнейшего поиска блока KeyEvents по файлу
-            eventCursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_START_BLOCK + SKIP_BYTE;
+            eventCursor = fileContent.lastIndexOf(compareToHex("KeyEvents")) + KEY_EVENTS_START_BLOCK + ONE_BYTE;
             countEvents = (int)compareToDec(hexScanner(fileContent, eventCursor, 1));
 
             // Первое событие находится около курсора, перемещаем его на безопасное расстояние
@@ -31,15 +35,20 @@ public class Main {
                 arrayEvent[i] = "";
                 hexNumber = compareToHex(i + 1, SHORT_INT_HEX);
                 eventCursor = cursor = fileContent.indexOf(hexNumber, eventCursor + KEY_EVENTS_OFFSET);
-                arrayEvent[i] += hexScanner(fileContent, cursor, SHORT_INT_HEX) + " ";
+                arrayEvent[i] += hexScanner(fileContent, cursor, SHORT_INT_HEX) + " "; // Порядковый номер
                 cursor += SHORT_INT_HEX;
-                arrayEvent[i] += hexScanner(fileContent, cursor, LONG_INT_HEX) + " ";
+                transitTime = compareToDec(flipHex(hexScanner(fileContent, cursor + ONE_BYTE, LONG_INT_HEX))) * Math.pow(10, -12); // Время распространения
+                arrayEvent[i] += (double)(Math.round(((transitTime * SPEED_OF_LIGHT) / refractionIndex) * 1000)) / 10000 + " "; // Расстояние до события
                 cursor += LONG_INT_HEX;
-                arrayEvent[i] += (double)compareToDec(hexScanner(fileContent, cursor, SHORT_INT_HEX)) / 1000 + " ";
+                arrayEvent[i] += (double)compareToDec(hexScanner(fileContent, cursor, SHORT_INT_HEX)) / 1000 + " "; // Километрическое затухание
+                cursor += SHORT_INT_HEX + ONE_BYTE;
+                if (hexScanner(fileContent, cursor + ONE_BYTE, ONE_BYTE).equals("FF")) { // Затухание на событии
+                    arrayEvent[i] += -(double)compareToDec(invertHex(flipHex(hexScanner(fileContent, cursor, SHORT_INT_HEX) + "FFFF"))) / 1000 + " ";
+                } else {
+                    arrayEvent[i] += (double)compareToDec(flipHex(hexScanner(fileContent, cursor, SHORT_INT_HEX))) / 1000 + " ";
+                }
                 cursor += SHORT_INT_HEX;
-                arrayEvent[i] += (double)compareToDec(hexScanner(fileContent, cursor, SHORT_INT_HEX)) / 1000 + " ";
-                cursor += SHORT_INT_HEX + SKIP_BYTE;
-                arrayEvent[i] += -(double)compareToDec(invertHex(flipHex(hexScanner(fileContent, cursor, LONG_INT_HEX)))) / 1000 + " ";
+                arrayEvent[i] += -(double)compareToDec(invertHex(flipHex(hexScanner(fileContent, cursor, LONG_INT_HEX)))) / 1000 + " "; // Коэффициент отражения
                 cursor += LONG_INT_HEX;
                 System.out.println("Событие №" + arrayEvent[i]);
 
@@ -74,7 +83,7 @@ public class Main {
     // Вывод N-ого количества символов из основной строки начиная с определенного значения
     public static String hexScanner(String fileContent, int cursor, int readBytes) {
         StringBuilder hexContent = new StringBuilder();
-        for (int i = 0; i < readBytes; i += SKIP_BYTE) {
+        for (int i = 0; i < readBytes; i += ONE_BYTE) {
             hexContent.append(fileContent.charAt(cursor + i));
             hexContent.append(fileContent.charAt(cursor + i + 1));
         }
@@ -99,7 +108,7 @@ public class Main {
     // Побайтовый переворот 16-ричного числа
     public static String flipHex(String hexNumber) {
         StringBuilder negativeHexNumber = new StringBuilder();
-        for (int i = hexNumber.length() - 1; i > 0; i-= SKIP_BYTE) {
+        for (int i = hexNumber.length() - 1; i > 0; i-= ONE_BYTE) {
             negativeHexNumber.append(hexNumber.charAt(i - 1));
             negativeHexNumber.append(hexNumber.charAt(i));
         }
