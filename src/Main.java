ad774941ed9b64406dc5fe7e1b40045a9dfd78a6
Andrew <ravski99@gmail.com>
2025-01-16 22:31:33 +0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,23 @@ import data.KeyEvents;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
-import static io.FileReader.*;
+
 import static io.FileSaver.*;
 import static processing.HexProcessing.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        int num;
+        String onePath;
         String path; // Путь к папке с файлами .sor
         String inputPath; // Путь до .sor-файла
-        String outputPath; // Путь вывода данных (..\output\.. .csv
+        String outputPath; // Путь вывода данных \..\output\.. .csv
         String fileContent; // Содержимое .sor-файла
-        FileSaver saveData; // Для сохранения данных в .csv
+        String[] sorFiles; // Список файлов .sor в выбранном каталоге
+        FileSaver saveData; // Сохранение данных в .csv
+        FileReader fileReader; // Определение файлов .sor в каталоге и чтение данных из них
         List<KeyEvents> eventContent; // Содержимое KeyEvent блока
         FxdParams fxdParamContent; // Содержимое FxdParams блока
 
@@ -25,28 +30,32 @@ public class Main {
         System.out.print("Введите путь к файлу (чтение): ");
         // path = console.readLine();
         path = scanner.nextLine();
-
-        String[] sorFiles = sorInDirectory(path);
+        fileReader = new FileReader();
+        sorFiles = fileReader.sorInDirectory(path);
         if (sorFiles.length != 0) {
+            fileReader.fiberNumberWidth(sorFiles);
             outputPath = path + "\\" + "output";
+            onePath = outputPath + "\\" + "ONE.csv";
             createDirectory(Path.of(outputPath));
             for (String file : sorFiles) {
                 inputPath = path + "\\" + file;
-                fileContent = FileReader.sorReader(inputPath);
+                fileContent = fileReader.sorReader(inputPath);
+                //num = fileReader.fiberNumber(file);
+                //System.out.println(num);
                 outputPath = path + "\\" + "output" + "\\" + file.substring(0, file.lastIndexOf(".sor")) + ".csv";
                 saveData = new FileSaver(outputPath);
-
                 if (fileContent != null) {
                     fxdParamContent = extractParameters(fileContent);
                     System.out.println(fxdParamContent);
                     eventContent = extractEvents(fileContent);
-                    saveData.writeInCSV(eventContent, outputPath);
+                    FileSaver.writeInOneCSV(eventContent, onePath);
+                    saveData.writeInCSV(eventContent);
                 } else {
                     System.out.println("Файл не найден или не удалось прочитать файл");
                 }
             }
         } else {
-            System.out.println("В директории нет файлов с расширением .sor");
+            System.out.println("В директории отсутствуют файлы с расширением .sor");
         }
     }
 
